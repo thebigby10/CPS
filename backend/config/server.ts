b9e8768871f1b218457f219e0f8d2306a4e89cbb
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-06-28 18:17:40 +0600

    .gitignore

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,27 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+
+  email: {
+    provider: "nodemailer",
+    settings: {
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_USERNAME"),
+        defaultReplyTo: env("SMTP_USERNAME"),
+      },
+    },
   },
 });
